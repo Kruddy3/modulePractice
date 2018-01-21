005ca23c9938ad4070a4c727001e16e62111f010
thestraded <thestraded@gmail.com>
2018-01-20 22:02:09 -0500

    first commit

---
+++ controllers/module.js
@@ -0,0 +1,18 @@
+// exports.myDateTime = function (val) {
+//     return val;
+// };
+// exports.moreTest = function (val) {
+//     return Date();
+// };
+
+
+
+module.exports.log = function(){
+  return {
+    dog: 'tipper'
+  }
+};
+// module.exports= {
+//   firstName: "James",
+//   lastName: "Bond"
+// }
---
+++ index.js
@@ -0,0 +1,17 @@
+var dt = require('./controllers/module');
+// console.log(dt.myDateTime('foo man choo'))
+// console.log(dt.moreTest())
+// dt.log('message logged ')
+
+//trying to get dog variable from a different file. i can import {dog} from './modus.js'
+
+
+//can I use fs? I remember using it in previous tutorials and such
+
+//i think its used for different type of file manipulation
+//typically json
+
+
+
+//this works but it doesnt seem like the best approach
+console.log(dt.log().dog)
